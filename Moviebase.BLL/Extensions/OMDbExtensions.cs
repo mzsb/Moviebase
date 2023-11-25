f26d988cd7cbba3a913c63c689cf52f797bcbc01
Mózs Barnabás <barnabas.mozs@gmail.com>
2023-11-25 02:09:42 +0100

    Decimal parse format fix

--- Moviebase.BLL/Extensions/OMDbExtensions.cs
+++ Moviebase.BLL/Extensions/OMDbExtensions.cs
@@ -2,6 +2,7 @@
 
 using Moviebase.BLL.Dtos;
 using Moviebase.DAL.Model;
+using System.Globalization;
 
 #endregion
 
@@ -9,12 +10,14 @@ namespace Moviebase.BLL.Extensions;
 
 public static class OMDbExtensions
 {
+    private readonly static NumberFormatInfo _numberFormatInfo = new(){ NumberDecimalSeparator = "." };
+
     public static Movie ToMovie(this OMDbDto movieData) => new()
     {
         Title = movieData.Title,
         Year = movieData.Year,
         PosterId = movieData.Poster.Split("/").Last().Split('.').First(),
-        ImdbRating = decimal.TryParse(movieData.ImdbRating , out var imdbRating) ?
+        ImdbRating = decimal.TryParse(movieData.ImdbRating, _numberFormatInfo, out var imdbRating) ?
             imdbRating : -1
     };
 }
