c439cf41e102bf6c1687c62e62326df14ae55bd2
Mózs Barnabás <barnabas.mozs@gmail.com>
2023-12-14 16:44:28 +0100

    CreateToken fix

--- Moviebase.BLL/Services/TokenService.cs
+++ Moviebase.BLL/Services/TokenService.cs
@@ -3,6 +3,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
+using Moviebase.BLL.Exceptions;
 using Moviebase.BLL.Extensions;
 using Moviebase.BLL.Interfaces;
 using Moviebase.DAL.Model.Identity;
@@ -18,34 +19,24 @@ public class TokenService(
     IConfiguration configuration, 
     UserManager<User> userManager) : ITokenService
 {
+    private readonly JwtSecurityTokenHandler _tokenHandler = new();
+
     private readonly SymmetricSecurityKey _tokenKey = new (Encoding.UTF8.GetBytes(configuration.GetValue("TokenKey")));
 
     private readonly UserManager<User> _userManager = userManager;
 
-    public async Task<string> CreateTokenAsync(User user)
-    {
-        List<Claim> claims = [
-            new (JwtRegisteredClaimNames.NameId, user.Id.ToString()),
-            new (JwtRegisteredClaimNames.UniqueName, user.UserName)
-        ];
-
-        var roles = await _userManager.GetRolesAsync(user);
-
-        claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
-
-        var creds = new SigningCredentials(_tokenKey, SecurityAlgorithms.HmacSha512Signature);
-
-        var tokenDescriptor = new SecurityTokenDescriptor
-        {
-            Subject = new ClaimsIdentity(claims),
-            Expires = DateTime.Now.AddDays(7),
-            SigningCredentials = creds
-        };
-
-        var tokenHandler = new JwtSecurityTokenHandler();
-
-        var token = tokenHandler.CreateToken(tokenDescriptor);
-
-        return tokenHandler.WriteToken(token);
-    }
+    public async Task<string> CreateTokenAsync(User user) => 
+        _tokenHandler.WriteToken(
+            _tokenHandler.CreateToken(new()
+            {
+                Subject = new ClaimsIdentity([
+                    new(JwtRegisteredClaimNames.NameId, user.Id.ToString()),
+                    new(JwtRegisteredClaimNames.UniqueName, user.UserName ?? throw new UserException("Invalid username")),
+                    .. (await _userManager.GetRolesAsync(user))
+                        .Select(role => new Claim(ClaimTypes.Role, role))
+                ]),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = new(_tokenKey, SecurityAlgorithms.HmacSha512Signature)
+            })
+        );
 }
